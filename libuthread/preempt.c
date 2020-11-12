83ec0d8192e556befb7cb6b7a0024d0edb9c2695
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2020-11-12 14:44:47 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,23 +15,49 @@
  */
 #define HZ 100
 
+struct sigaction act;
+struct itimerval timer;
+struct sigaction act_old;
+struct itimerval timer_old;
+
+void handler() {
+  uthread_yield();
+}
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
+  sigprocmask(SIG_BLOCK, &act.sa_mask, &act_old.sa_mask);
 }
 
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
+  sigprocmask(SIG_UNBLOCK, &act.sa_mask, &act_old.sa_mask);
 }
 
 void preempt_start(void)
 {
 	/* TODO Phase 4 */
+  sigemptyset(&act.sa_mask);
+  sigaddset(&act.sa_mask, SIGVTALRM);
+
+  act.sa_handler = &handler;
+  sigaction(SIGVTALRM, &act, &act_old);
+
+  timer.it_value.tv_sec = 0;
+  timer.it_value.tv_usec = 1000000/HZ;
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_usec = 1000000/HZ;
+  setitimer(ITIMER_VIRTUAL, &timer, &timer_old);
 }
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = SA_RESETHAND;
+  sigaction(SIGVTALRM, &act_old, NULL);
+  setitimer(ITIMER_REAL, &timer_old, NULL);
 }
 

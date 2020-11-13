21232da934d7160677e1cf0897ab25ace6f8cf42
aceace001 <72894562+aceace001@users.noreply.github.com>
2020-11-12 23:46:59 -0800

    Update preempt.c

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -22,44 +22,43 @@ struct sigaction act_old;
 struct itimerval timer_old;
 
 void handler() {
-  uthread_yield();
+	uthread_yield();
 }
 
 void preempt_disable(void)
 {
-  // block sigaction
-  sigprocmask(SIG_BLOCK, &act.sa_mask, &act_old.sa_mask);
+	// block sigaction
+	sigprocmask(SIG_BLOCK, &act.sa_mask, &act_old.sa_mask);
 }
 
 void preempt_enable(void)
 {
-// unblock sigaction
-  sigprocmask(SIG_UNBLOCK, &act.sa_mask, &act_old.sa_mask);
+	// unblock sigaction
+	sigprocmask(SIG_UNBLOCK, &act.sa_mask, &act_old.sa_mask);
 }
 
 void preempt_start(void)
 {
-  // initialize a signal handler and a timer that
-  // force the current running thread to yield
-  sigemptyset(&act.sa_mask);
-  sigaddset(&act.sa_mask, SIGVTALRM);
+	// initialize a signal handler and a timer that
+	// force the current running thread to yield
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask, SIGVTALRM);
 
-  act.sa_handler = &handler;
-  sigaction(SIGVTALRM, &act, &act_old);
+	act.sa_handler = &handler;
+	sigaction(SIGVTALRM, &act, &act_old);
 
-  timer.it_value.tv_sec = 0;
-  timer.it_value.tv_usec = 1000000/HZ;    // 10000 microseconds = 0.01 seconds
-  timer.it_interval.tv_sec = 0;
-  timer.it_interval.tv_usec = 1000000/HZ;
-  setitimer(ITIMER_VIRTUAL, &timer, &timer_old);
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 1000000/HZ;    // 10000 microseconds = 0.01 seconds
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 1000000/HZ;
+	setitimer(ITIMER_VIRTUAL, &timer, &timer_old);
 }
 
 void preempt_stop(void)
 {
-  // restore previous sigaction and timer
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = SA_RESETHAND;
-  sigaction(SIGVTALRM, &act_old, NULL);
-  setitimer(ITIMER_REAL, &timer_old, NULL);
+	// restore previous sigaction and timer
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_RESETHAND;
+	sigaction(SIGVTALRM, &act_old, NULL);
+	setitimer(ITIMER_REAL, &timer_old, NULL);
 }
-

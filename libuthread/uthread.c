11ba24afb42a6640acdbea2a402920fe50520274
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2020-11-12 16:08:19 -0800

    Update uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -12,6 +12,8 @@
 
 enum states { RUN, READY, BLOCKED, ZOMBIE };
 
+// thread control block
+// contains a thread's context, stack pointer, and state
 struct uthread_tcb {
   uthread_ctx_t *ctx;
   void *stack;
@@ -33,6 +35,8 @@ void uthread_yield(void) {
   preempt_disable();
   prev_thread = curr_thread;
 
+  // put current thread into ready queue
+  // context switch to the next thread in line
   queue_enqueue(ready_queue, curr_thread);
   queue_dequeue(ready_queue, (void**) &next_thread);
 
@@ -45,14 +49,17 @@ void uthread_yield(void) {
 }
 
 void uthread_exit(void) {
-  if (queue_length(ready_queue) > 0){
+  // free current exiting thread's stack
+  curr_thread->state = ZOMBIE;
+  uthread_ctx_destroy_stack(curr_thread->stack);
+
+  // if there are still threads in ready queue,
+  // context switch to the next one in line
+  if (queue_length(ready_queue) > 0) {
     struct uthread_tcb* next_thread = (struct uthread_tcb *) malloc(sizeof(struct uthread_tcb));
 
     preempt_disable();
     queue_dequeue(ready_queue, (void**) &next_thread);
-
-    curr_thread->state = ZOMBIE;
-    uthread_ctx_destroy_stack(curr_thread->stack);
 
     preempt_enable();
     uthread_ctx_switch(curr_thread->ctx, next_thread->ctx);
@@ -67,6 +74,7 @@ int uthread_create(uthread_func_t func, void *arg) {
   new_thread->stack = uthread_ctx_alloc_stack();
   new_thread->state = READY;
 
+  // newly created thread are enqueued in ready queue
   queue_enqueue(ready_queue, new_thread);
 
   preempt_enable();
@@ -75,6 +83,7 @@ int uthread_create(uthread_func_t func, void *arg) {
 }
 
 int uthread_start(uthread_func_t func, void *arg) {
+  // create the main idle thread
   struct uthread_tcb* idle_thread = (struct uthread_tcb*) malloc (sizeof(struct uthread_tcb));
   idle_thread->ctx = malloc(sizeof(uthread_ctx_t));
   if (idle_thread->ctx == NULL) {
@@ -86,20 +95,26 @@ int uthread_start(uthread_func_t func, void *arg) {
   }
   idle_thread->state = RUN;
 
-  uthread_ctx_init(idle_thread->ctx, idle_thread->stack, func, arg);
+  // uthread_ctx_init(idle_thread->ctx, idle_thread->stack, func, arg);
   curr_thread = idle_thread;
 
+  // initialize the queues
   ready_queue = queue_create();
   block_queue = queue_create();
 
   preempt_start();
   preempt_enable();
 
+  // create the first function thread
   uthread_create(func, arg);
 
+  // idle thread yields until there are no more threads in ready queue
   while (queue_length(ready_queue) > 0) {
     uthread_yield();
   }
+
+  preempt_stop();
+  // destroy the queues
   queue_destroy(ready_queue);
   queue_destroy(block_queue);
   return 0;
@@ -108,20 +123,23 @@ int uthread_start(uthread_func_t func, void *arg) {
 void uthread_block(void) {
   preempt_disable();
   struct uthread_tcb* next_thread = (struct uthread_tcb *) malloc(sizeof(struct uthread_tcb));
+  // enqueue current thread into the block queue
   queue_enqueue(block_queue, curr_thread);
   curr_thread->state = BLOCKED;
 
+  // context switch to the next ready thread
   queue_dequeue(ready_queue, (void**) &next_thread);
   next_thread->state = RUN;
 
+  preempt_enable();
   uthread_ctx_switch(curr_thread->ctx, next_thread->ctx);
   curr_thread = next_thread;
-  preempt_enable();
 }
 
 void uthread_unblock(struct uthread_tcb *uthread) {
   preempt_disable();
   uthread->state = READY;
+  // remove uthread from block queue and put it into ready queue instead
   queue_dequeue(block_queue, (void**) &uthread);
   queue_enqueue(ready_queue, uthread);
   preempt_enable();

35248e627021f68252a913943ed93b2dac1a1481
aceace001 <72894562+aceace001@users.noreply.github.com>
2020-11-12 23:25:22 -0800

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -5,84 +5,92 @@
 #include "sem.h"
 #include "private.h"
 
+//struct to keep track of resources, and have a lock to implement test and set function
 struct semaphore {
-  size_t count;
-  queue_t queue_block;
-  int lock;
+	size_t count;
+	queue_t queue_block;
+	int lock;
 };
 
+//test and set function to implement lock to use in sem_up and sem_down 
 int test_and_set (int *mem) {
-  int oldval = *mem;
-  *mem = 1;
+	int oldval = *mem;
+	*mem = 1;
 
-  return oldval;
+	return oldval;
 }
 
+//initialize queue for semaphore
 sem_t sem_create(size_t count)
 {
-  sem_t sem = (sem_t) malloc(sizeof(struct semaphore));
+	//allocate spapce for sem
+	sem_t sem = (sem_t) malloc(sizeof(struct semaphore));
 
-  sem->count = count;
-  sem->queue_block = queue_create();
-  sem->lock = 0;
+	sem->count = count;
+	sem->queue_block = queue_create();
+	sem->lock = 0;
 
-  return sem;
+	return sem;
 }
 
 int sem_destroy(sem_t sem)
 {
-  if (sem == NULL || queue_length(sem->queue_block) > 0) {
-    return -1;
-  }
+	//if sem is NULL or the queue of sem is not empty, then cannot destroy
+	if (sem == NULL || queue_length(sem->queue_block) > 0) {
+		return -1;
+	}
 
-  free(sem);
+	free(sem);
 
-  return 0;
+	return 0;
 }
 
-
+//uses test and set function to implement spinlock 
+//if count is 0, meaning there is no resources available, thread is block
+//until count is not 0.
 int sem_down(sem_t sem)
 {
-  struct uthread_tcb *thread = uthread_current();
+	struct uthread_tcb *tid = uthread_current();
 
-  if (sem == NULL) {
-    return -1;
-  }
+	//cannot sem down if sem is NULL
+	if (sem == NULL) {
+		return -1;
+	}
 
-  while (test_and_set(&sem->lock) == 1){
-    while (sem->count == 0) {
-      queue_enqueue(sem->queue_block, &thread);
-      uthread_block();
-    }
+	while (test_and_set(&sem->lock) == 1){
+		while (sem->count == 0) {
+		queue_enqueue(sem->queue_block, &tid);
+		uthread_block();
+		}
 
-    sem->count -=1;
-  }
+	sem->count -=1;
+	}
 
-  sem->lock = 0;
+	sem->lock = 0;
 
-  return 0;
+	return 0;
 
 }
 
+//also uses test and set function to implement spinlock
 int sem_up(sem_t sem)
 {
-  struct uthread_tcb *thread;
+	struct uthread_tcb *tid;
 
-  if (sem == NULL) {
-    return -1;
-  }
+	//cannot sem up if sem is NULL
+	if (sem == NULL) {
+		return -1;
+	}
+	
+	while (test_and_set(&sem->lock) == 1) {
+		sem->count += 1;
+		if (queue_length(sem->queue_block) > 0) {
+			queue_dequeue(sem->queue_block, (void*)&tid);
+			uthread_unblock(tid);
+		}
+	}
 
-  while (test_and_set(&sem->lock) == 1) {
-    sem->count += 1;
-    if (queue_length(sem->queue_block) > 0) {
-      queue_dequeue(sem->queue_block, (void**) &thread);
-      uthread_unblock(thread);
-    }
-  }
+	sem->lock = 0;
 
-  sem->lock = 0;
-
-  return 0;
+	return 0;
 }
-
-

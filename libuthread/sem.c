c33e68f3e42821cb2792b361e6230e9d4b9f0b69
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2020-11-12 21:14:21 -0800

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "queue.h"
 #include "sem.h"
@@ -8,22 +9,48 @@
 struct semaphore {
   size_t count;
   queue_t queue_block;
+  int lock;
 };
 
-sem_t sem_create(size_t count) {
+int test_and_set (int *mem) {
+  int oldval = *mem;
+  *mem = 1;
 
+  return oldval;
 }
 
-int sem_destroy(sem_t sem) {
+sem_t sem_create(size_t count)
+{
+  sem_t sem = (sem_t) malloc(sizeof(struct semaphore));
 
+  sem->count = count;
+  sem->queue_block = queue_create();
+  sem->lock = 0;
+
+  return sem;
 }
+
+int sem_destroy(sem_t sem)
+{
+  if (sem == NULL || queue_length(sem->queue_block) > 0) {
+    return -1;
+  }
+
+  return 0;
+}
+
 
 int sem_down(sem_t sem)
 {
+  sem->lock = 0;
+  return 0;
 
 }
 
 int sem_up(sem_t sem)
 {
-
+  sem->lock = 0;
+  return 0;
 }
+
+

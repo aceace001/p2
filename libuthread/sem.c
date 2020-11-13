be56acbfaf2a757d9f0e0b127ff5d97d66da57d6
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2020-11-12 21:19:56 -0800

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 #include "queue.h"
 #include "sem.h"
@@ -36,20 +35,53 @@ int sem_destroy(sem_t sem)
     return -1;
   }
 
+  free(sem);
+
   return 0;
 }
 
 
 int sem_down(sem_t sem)
 {
+  struct uthread_tcb *thread = uthread_current();
+
+  if (sem == NULL) {
+    return -1;
+  }
+
+  while (test_and_set(&sem->lock) == 1){
+    while (sem->count == 0) {
+      queue_enqueue(sem->queue_block, &thread);
+      uthread_block();
+    }
+
+    sem->count -=1;
+  }
+
   sem->lock = 0;
+
   return 0;
 
 }
 
 int sem_up(sem_t sem)
 {
+  struct uthread_tcb *thread;
+
+  if (sem == NULL) {
+    return -1;
+  }
+
+  while (test_and_set(&sem->lock) == 1) {
+    sem->count += 1;
+    if (queue_length(sem->queue_block) > 0) {
+      queue_dequeue(sem->queue_block, (void**) &thread);
+      uthread_unblock(thread);
+    }
+  }
+
   sem->lock = 0;
+
   return 0;
 }
 
